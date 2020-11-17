aeac2b67d41b4e527e9761f9939d4f3d04309f72
Saurabh <saurabh.nit20@gmail.com>
2020-11-18 02:14:27 +0530

    [Saurabh].Read Entries from JSON Server

---
+++ AddressBook.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookforRestSharpTesting
+{
+    public class AddressBook
+    {
+        public int pin { get; set; }
+        public string name { get; set; }
+        public string city { get; set; }
+
+    }
+}
--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,13 +1,51 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Net;
 
 namespace AddressBookforRestSharpTesting
 {
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        RestClient client;                                                      //Declaration of Client
+
+        [TestInitialize]
+        public void SetUp()
         {
+            client = new RestClient("http://localhost:4000");                      //Setting Link for the Client 
         }
+
+        private IRestResponse getAddressList()
+        {
+            //arrange
+            RestRequest request = new RestRequest("/AddressBook", Method.GET);              //Retrieving EmployeesList 
+
+            //act
+            IRestResponse response = client.Execute(request);
+            return response;
+        }
+
+
+        [TestMethod]
+        public void OnCallingReturnEmployeeList()
+        {
+            IRestResponse response = getAddressList();
+
+            //Assert
+            Assert.AreEqual(response.StatusCode, HttpStatusCode.OK);                                 //Checking the Status
+            List<AddressBook> dataResponse = JsonConvert.DeserializeObject<List<AddressBook>>(response.Content);          //Desrializing Object
+            Assert.AreEqual(3, dataResponse.Count);                                                          //Counting the responses that matches
+
+
+            //Retrieving the data from the DataResponse 
+            foreach (var item in dataResponse)
+            {
+                Console.WriteLine("Pin Code: " + item.pin + " Name: " + item.name + " City: " + item.city);
+            }
+        }
+
     }
 }
